04c452a412afd515fb472d203daf23cfcd4a261c
SuperThunder <alexander.wolfe.10@googlemail.com>
2016-04-07 14:29:51 -0400

    Example pictures

--- datetimeConvert.py
+++ datetimeConvert.py
@@ -2,15 +2,15 @@ import datetime
 import pandas as pd
 
 # Converts the datetime string to a different format
-def toHour(csvName, format):
+def toHour(csvName, dtFormat):
     oldCSV = csvName + '.csv'
     newCSV = csvName + 'Hours' + '.csv'
-    if format == 'def':
-        format = "%Y %m %d %H %M %S"
+    if dtFormat == 'def':
+        dtFormat = "%Y%m%d%H%M%S"
 
     # Open the source CSV and also parse all the datetimes in column 3 (PollTime)
     source = pd.read_csv(oldCSV, parse_dates=[2])
     # Writes the CSV to a new file with the datetimes in a different format
-    source.to_csv(newCSV, mode='wb', date_format=format)
+    source.to_csv(newCSV, mode='wb', date_format=dtFormat)
 
 toHour("sample2.dbCSV", "%H")
