19474cfecae5130923d8172be675753bc56f546f
SuperThunder <alexander.wolfe.10@googlemail.com>
2016-04-07 09:26:59 -0400

    toCSV now adds column headers. Started test file for pandas library.

--- pandas_test1.py
+++ pandas_test1.py
@@ -8,6 +8,12 @@ import matplotlib.pylab as plt
 def pdt1(csvSource):
     csvFileName = csvSource + '.csv'
     dataFile = pd.read_csv(csvFileName)
+    print dataFile
+
+    '''
     print dataFile.names
     for row in dataFile:
         print row[0]
+    '''
+
+pdt1("sample2.dbCSV")
