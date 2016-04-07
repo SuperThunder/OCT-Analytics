78348ed7ff5705720b270dca3a9cf837264250c9
SuperThunder <alexander.wolfe.10@googlemail.com>
2016-04-07 13:09:09 -0400

    Datetime converison of CSV using pandas now working and good.

--- pandas_test1.py
+++ pandas_test1.py
@@ -8,12 +8,9 @@ import matplotlib.pylab as plt
 def pdt1(csvSource):
     csvFileName = csvSource + '.csv'
     dataFile = pd.read_csv(csvFileName)
-    print dataFile
+    columnNames = list(dataFile.columns.values)
+    print columnNames
 
-    '''
-    print dataFile.names
-    for row in dataFile:
-        print row[0]
-    '''
+
 
 pdt1("sample2.dbCSV")
