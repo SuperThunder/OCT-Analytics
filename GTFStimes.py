119ca020538062b63ddb6dbc94647543554ff4a6
SuperThunder <alexander.wolfe.10@googlemail.com>
2016-04-11 19:44:21 -0400

    Working GTFS parser, except for sorting seemingly random times

--- GTFStimes.py
+++ GTFStimes.py
@@ -4,18 +4,24 @@ import csv
 def makeTimesDB(csvName, stopID, route):
     outputName = csvName + stopID + 'Times'
     with open(csvName, 'rb') as source:
-        with open(outputName, 'wb') as dest:
+        with open('GTFSScheduledTimes'+stopID+'-'+route+'.csv', 'wb') as dest:
             csvReader = csv.reader(source)
             csvWriter = csv.writer(dest)
 
+            valList = []
+            valList.append([])  # Can probably replace this with a list comprehension in the declaration
+            valList.append([])
+            tripList = []
+            timeList = []
             for row in csvReader:
                 if row[3] == stopID:
-                    print row[0], row[1]
+                    #print row[0], row[1]
+                    #tripList.append(row[0])  # Add the trip code
+                    #timeList.append(row[1])  # Add the scheduled time
+                    valList[0].append(row[0])
+                    valList[1].append(row[1])
 
-                '''
-                if i == 0:
-                    print row
-                i +=1
-                '''
+            print sorted([tuple(map(int, d.split(":"))) for d in valList[1]])
+
 
 makeTimesDB('./google_transit/stop_times.csv', 'AA060', '9')
