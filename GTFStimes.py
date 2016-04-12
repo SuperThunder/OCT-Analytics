14e9ae516570cc39cfebfca86f2b4d496ed5ce66
SuperThunder <alexander.wolfe.10@googlemail.com>
2016-04-12 14:52:20 -0400

    GTFS parser now makes CSV file. No sorting yet.

--- GTFStimes.py
+++ GTFStimes.py
@@ -8,20 +8,38 @@ def makeTimesDB(csvName, stopID, route):
             csvReader = csv.reader(source)
             csvWriter = csv.writer(dest)
 
-            valList = []
-            valList.append([])  # Can probably replace this with a list comprehension in the declaration
-            valList.append([])
+
+            #valList.append([])  # Can probably replace this with a list comprehension in the declaration
+            #valList.append([])
+            #valDict = {}
+            #valDict
             tripList = []
             timeList = []
             for row in csvReader:
                 if row[3] == stopID:
                     #print row[0], row[1]
-                    #tripList.append(row[0])  # Add the trip code
-                    #timeList.append(row[1])  # Add the scheduled time
-                    valList[0].append(row[0])
-                    valList[1].append(row[1])
+                    csvRow = row[0], row[1]
+                    tripList.append(row[0])  # Add the trip code
+                    timeList.append(row[1])  # Add the scheduled time
+                    csvWriter.writerow([row[0], row[1]])
 
-            print sorted([tuple(map(int, d.split(":"))) for d in valList[1]])
+                    #valList[0].append(row[0])
+                    #valList[1].append(row[1])
+                    #valList.append({row[0]: row[1]})
+                    #addToDict(valDict, row[0], row[1])
 
+            #print valDict
+
+            #print sorted([tuple(map(int, d.split(":"))) for d in valList[1]])
+
+# http://stackoverflow.com/questions/18817789/how-to-add-values-to-existing-dictionary-key-python
+def addToDict(valDict, key, value):
+    if key in valDict:
+        valDict[key].update({key:value})
+    else:
+        valDict[key] = value
+
+
+    #return valDict
 
 makeTimesDB('./google_transit/stop_times.csv', 'AA060', '9')
