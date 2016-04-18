9e606d82ec65593b11752cdc29ed846bb89e8e13
SuperThunder <alexander.wolfe.10@googlemail.com>
2016-04-18 14:57:13 -0400

    Started attribute splitter and difference comparer

--- checkSchVsEst.py
+++ checkSchVsEst.py
@@ -3,6 +3,19 @@ import csv
 
 # Split data into as small attributes as possible and find difference between scheduled time and estimate at that time
 def schVsEst(liveCSV, scheduleCSV):
-    with open(liveCSV+'.csv','rb') as liveData:
-        with open(scheduleCSV+'.csv','rb') as schData:
-            with open('Attributes'+liveCSV+scheduleCSV, 'wb') as dest:
+    with open(liveCSV+'.csv', 'rb') as liveData:
+        with open(scheduleCSV+'.csv', 'rb') as schData:
+            with open('Attributes'+liveCSV+scheduleCSV+'.csv', 'wb') as dest:
+                csvLiveReader = csv.reader(liveData)
+                csvSchReader = csv.reader(schData)
+                csvOutput = csv.writer(dest)
+                headerRow = ['StopNum', 'RouteNum', 'PollTimeYear', 'PollTimeMonth', 'PollTimeMonthNum',
+                             'PollTimeWeekday', 'PolLTimeDay', 'PolLTimeHour', 'PollTimeMinute', 'PollTimeSecond',
+                             'TimeToNext', 'TimeTo2nd' 'GTFSTrip', 'GTFSServiceID', 'SchTime', 'TimeDiff']
+                csvOutput.writeline(headerRow)
+
+                destRow = []
+                #get first and last date in live data?
+
+
+schVsEst('sample2.dbCSV', 'GTFSScheduledTimesAA060-9')
