c7d5fbaea405994c9c1426ab19208f07d038b9ed
SuperThunder <alexander.wolfe.10@googlemail.com>
2016-04-21 13:42:05 -0400

    In process of going generating all planned arrival times in the range and then comparing if the arrival was on time

--- checkSchVsEst.py
+++ checkSchVsEst.py
@@ -1,6 +1,6 @@
 import datetime
 import csv
-
+from collections import defaultdict
 
 # Split data into as small attributes as possible and find difference between scheduled time and estimate at that time
 def schVsEst(liveCSV, scheduleCSV):
@@ -67,12 +67,40 @@ def splitIntoAttributes(liveData, liveAttribData):
 # need to iterate through the scheduled times in the time for which we have live times
 # return them as a dictionary of a date to its scheduled times
 def getScheduledTimes(startDate, endDate, scheduleCSV):
+    exclDates = genExclusionDays()
+    print "Exclusion dates: ", exclDates
+    tripDates = retDates()
+    print 'Trip date ranges: ', tripDates
     with open(scheduleCSV+'.csv', 'rb') as schData:
+
         print ''
 
 def genExclusionDays():
-    return 0
+    with open('./google_transit/calendar_dates.txt', 'rb') as exclDates:
+        exclDatesCSV = csv.reader(exclDates)
+        #exclDatesDicts = []
+        exclDatesDict = defaultdict(list)
+        next(exclDatesCSV, None)  # skip header
+        for row in exclDatesCSV:
+            exclDatesDict[row[0]].append(row[1])
 
+    return exclDatesDict
+
+# Hardcoded for specific busses, won't work with everything
+def retDates():
+    with open('./google_transit/calendar.txt', 'rb') as trips:
+        tripsCSV = csv.reader(trips)
+        tripDates = defaultdict(list)
+        next(tripsCSV, None)  # skip header
+        # luckily only need first 6 rows of data
+        i = 0
+        for row in tripsCSV:
+            print i, row
+            if i <= 5:
+                tripDates[row[0]].append([row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9]])
+            i += 1
+
+        return tripDates
 
 
 schVsEst('sample2.dbCSV', 'GTFSScheduledTimesAA060-9')
