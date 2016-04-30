a8eeb248c676d73b3bb60fde09efa3be0b009428
SuperThunder <alexander.wolfe.10@googlemail.com>
2016-04-30 17:51:19 -0400

    testing analysis stuff

--- sklearn_test1.py
+++ sklearn_test1.py
@@ -1,26 +1,43 @@
 from sklearn import svm # no idea why but if sklearn is imported sklearn.svm doesn't work
 import numpy as np
-import csv
 from collections import defaultdict
 
 SAMPLE_NAME = 'sample5 Attributes and Time Discrepencies t-0mins'
 
-listdict = defaultdict(list)
 with open(SAMPLE_NAME+'.csv', 'r') as src:
-    #next(src)
     # columns we want to load from the csv
     colstouse = [5, 7, 10, 14]
     # numpy txt loading command, note parameters to handle headers and missing values
     data = np.genfromtxt(fname=src, names=True, delimiter=',', missing_values=['-100', '-50', ''],
                          filling_values=None, usecols=colstouse, skip_header=0, dtype=int)
-
+    X = [data['PollTimeWeekday'], data['PollTimeHour'], data['TimeToNext']]
 colnames = data.dtype.names
-
 print colnames
-print data
+print X
 
-classifier = svm.SVC(gamma=0.001, C=100.) # create an 'estimator instance'
-#classifier.fit(X=)
+'''
+Quite a lot of transformations had to be made to bludgeon the data into working
+1. we take the 3 input columns lists and put them in a list (X)
+2. this list is turned into a matrix (Xm)
+3. this matrix is turned back into an ndarray, then reshaped to be the same dimensions as y
+'''
+Xm = np.matrix(X)
+#XmA =  Xm.A.reshape(1101,3) # below command should do this but without needing to be adjusted
+XmA =  Xm.A.reshape(len(data['Discrepancy']), len(colnames)-1)
+print XmA
+
+
+#X = np.array(data['TimeToNext'])
+
+
+
+
+
+
+classifier = svm.SVC(gamma=0.001, C=100.)  # create an 'estimator instance'
+classifier.fit(X=XmA, y=data['Discrepancy'])
+print classifier
+print classifier.predict(XmA)
 
 
 '''
