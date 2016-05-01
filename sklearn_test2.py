1f5499e01dba75ac64a5fd6ba328af7cca464396
SuperThunder <alexander.wolfe.10@googlemail.com>
2016-05-01 10:59:18 -0400

    testing analysis stuff

--- sklearn_test2.py
+++ sklearn_test2.py
@@ -2,6 +2,15 @@ from sklearn import svm
 import numpy as np
 import csv
 
+def writeresults(clfname, clf):
+    with open(clfname+' for '+SAMPLE_NAME+'.csv', 'wb') as dest:
+        destwriter = csv.writer(dest)
+        destwriter.writerow(['Weekday', 'Hour', 'Predicted Discrepancy'])
+        for weekday in range(1, 7+1):
+            for hour in range(6, 23+1):
+                destwriter.writerow([weekday, hour, clf.predict([weekday, hour])[0]])
+            destwriter.writerow(['', '', ''])
+
 SAMPLE_NAME = 'sample5 Attributes and Time Discrepencies t-1mins'
 with open(SAMPLE_NAME+'.csv', 'r') as src:
     # columns we want to load from the csv
@@ -25,15 +34,15 @@ print 'Output len ', len(output)
 # http://scikit-learn.org/stable/modules/svm.html
 clf = svm.SVC()
 clf.fit(X=input, y=output)
+print np.array(input).shape
+print np.array(output).shape
+print 'Writing SVM results'
+writeresults('SVM Predictions', clf)
 
-with open('SVM Prediction '+SAMPLE_NAME+'.csv', 'wb') as dest:
-    destwriter = csv.writer(dest)
-    destwriter.writerow(['Weekday', 'Hour', 'Predicted Discrepancy'])
-    for weekday in range(1, 7+1):
-        for hour in range(6, 23+1):
-            destwriter.writerow([weekday, hour, clf.predict([weekday, hour])[0]])
-        destwriter.writerow(['', '', ''])
-
+regclf = svm.SVR()
+regclf.fit(X=input, y=output)
+print 'Writing SVR results'
+writeresults('SVR Predictions', regclf)
 
 
 '''
