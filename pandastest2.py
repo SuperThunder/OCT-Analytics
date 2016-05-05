c42d116610f4e24eadc30aab2e2d84982e7c5277
SuperThunder <alexander.wolfe.10@googlemail.com>
2016-05-05 14:16:39 -0400

    Started plotting test with pandas and matplotlib

--- pandastest2.py
+++ pandastest2.py
@@ -1,13 +1,17 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-FILE_NAME = 'sample5CSV.csv'
-with open(FILE_NAME, 'rb') as sample:
-    data = pd.read_csv(sample, parse_dates=True, na_values=['-50','-100'])
-    indexes =  list(range(len(data)))
-    # Can potentially add a color spectrum to this
-    plt.plot(indexes, data['TimeToNext'], linestyle='None', marker='o', color='g')
-    plt.xlabel('Index')
-    plt.ylabel('TimeToNext')
-    plt.title('Index vs TimeToNext')
-    plt.show()
+def plottest(FILE_NAME, xlabel, ylabel, title, key):
+    with open(FILE_NAME, 'rb') as sample:
+        data = pd.read_csv(sample, parse_dates=True, na_values=['-50','-100'])
+        indexes = list(range(len(data)))  # Create a list of the indexes to use to graph the time series
+        # Can potentially add a color spectrum to this
+        plt.plot(indexes, data[key], marker='o', color='g', markeredgecolor='None')
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.title(title)
+        plt.show()
+
+plottest('sample5CSV.csv', 'Index', 'TimeToNext', 'Index vs TimeToNext', 'TimeToNext')
+plottest('./Machine learning results/SVR Predictions for sample5 Attributes and Time Discrepencies t-1mins.csv',
+         'Index', 'Expected Discrepancy', 'Predicted Discrepancy vs Weekdays', 'Predicted Discrepancy')
