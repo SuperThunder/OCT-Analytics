3871fd149347128c09f7c94d6b01715233ba88f2
SuperThunder <alexander.wolfe.10@googlemail.com>
2016-05-05 22:10:49 -0400

    Started plotting test with pandas and matplotlib

--- pandastest2.py
+++ pandastest2.py
@@ -1,17 +1,28 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def plottest(FILE_NAME, xlabel, ylabel, title, key):
+def plotpoints(FILE_NAME, xlabel, ylabel, title, key):
     with open(FILE_NAME, 'rb') as sample:
         data = pd.read_csv(sample, parse_dates=True, na_values=['-50','-100'])
         indexes = list(range(len(data)))  # Create a list of the indexes to use to graph the time series
         # Can potentially add a color spectrum to this
-        plt.plot(indexes, data[key], marker='o', color='g', markeredgecolor='None')
+        plt.plot(indexes, data[key], marker='o', color='g', markeredgecolor='None', linewidth=0)
         plt.xlabel(xlabel)
         plt.ylabel(ylabel)
         plt.title(title)
         plt.show()
 
-plottest('sample5CSV.csv', 'Index', 'TimeToNext', 'Index vs TimeToNext', 'TimeToNext')
-plottest('./Machine learning results/SVR Predictions for sample5 Attributes and Time Discrepencies t-1mins.csv',
+def plotlines(FILE_NAME, xlabel, ylabel, title, key):
+    with open(FILE_NAME, 'rb') as sample:
+        data = pd.read_csv(sample, parse_dates=True, na_values=['-50','-100'])
+        indexes = list(range(len(data)))  # Create a list of the indexes to use to graph the time series
+        # Can potentially add a color spectrum to this
+        plt.plot(indexes, data[key], marker='o', color='g', markeredgecolor='None', linewidth = 5)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.title(title)
+        plt.show()
+
+plotpoints('sample5CSV.csv', 'Index', 'TimeToNext', 'Index vs TimeToNext', 'TimeToNext')
+plotlines('./Machine learning results/SVR Predictions for sample5 Attributes and Time Discrepencies t-1mins.csv',
          'Index', 'Expected Discrepancy', 'Predicted Discrepancy vs Weekdays', 'Predicted Discrepancy')
